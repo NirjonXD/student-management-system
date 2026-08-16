f950696f7ceda215f25af069161e1451208851db
NirjonXD <2023-2-60-113@std.ewubd.edu>
2026-08-17 02:46:34 +0600

    fixed some error and completed update function successfully

--- sms.cpp
+++ sms.cpp
@@ -28,11 +28,11 @@ class Linked_List{
             newNode->roll_no=r;
             newNode->name=n;
             newNode->marks=m;
-            newNode->per=m/(100*100);
+            newNode->per=(m/100)*100;
             newNode->next=nullptr;
 
             if(head==nullptr){
-                head==newNode;
+                head=newNode;
             }
             else{
                 Node* temp=head;
@@ -72,13 +72,56 @@ class Linked_List{
         }
 
         void count(){
-            
+            if(head==nullptr){
+                cout<<"\n\nLINKED LIST IS EMPTY!";
+            }
+            else{
+                int cnt=0;
+                Node* temp=head;
+                while(temp!=nullptr){
+                    cnt++;
+                    temp=temp->next;
+                }
+                cout<<"\n\n TOTAL NUMBER OF NODES : "<<cnt<<endl;
+            }
         }
+
+        void update(){
+              if(head==nullptr){
+                cout<<"\n\nLINKED LIST IS EMPTY!!!";
+              }  
+              else{
+                int r,found=0;
+                cout<<"\n\n ENTER ROLL NO. : ";
+                cin>>r;
+                Node* temp=head;
+                while(temp!=nullptr){
+                    if(r==temp->roll_no){
+                        cout<<"\n\nENTER NEW ROLL NO. : ";
+                        cin>>temp->roll_no;
+                        cout<<"\n\n ENTER NAME : ";
+                        cin>>temp->name;
+                        cout<<"\n\n ENTER MARKS : ";
+                        cin>>temp->marks;
+                        temp->per=temp->marks;
+                        cout<<"\n\nRECORD UPDATED SUCCESSFULLY!";
+                        found++;
+                        break;
+                    }
+                    temp=temp->next;
+                }
+                if(found==0){
+                    cout<<"\n\nNO RESULTS FOUND!!!";
+                }
+              }
+        }
+
 
 };
 
 int main(){ 
     int choice;
+    Linked_List obj;
 
     p:
     system("cls");
@@ -94,19 +137,24 @@ int main(){
     cout<<"\n\n6.SHOW ALL RECORD";
     cout<<"\n\n7.EXIT";
     cout<<"\n\n---------------------------------------------------";
+    cout<<"\n\nPLEASE CHOOSE YOUR OPTION : ";
     cin>>choice;
     switch(choice){
         case 1:
         system("cls");
+        obj.insert();
         break;
         case 2:
         system("cls");
+        obj.search();
         break;
         case 3:
         system("cls");
+        obj.count();
         break;
         case 4:
         system("cls");
+        obj.update();
         break;
         case 5:
         system("cls");
